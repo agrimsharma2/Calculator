437735e3dbc1a805469af5239304d082bc762ec8
Mortaza Yari <mortazayari@Mortazas-MacBook-Air.local>
2022-10-18 13:28:50 +1100

    feat: change dir

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,5 @@ deploymentGroupId=$(aws deploy get-deployment-group --application-name calculato
 deploymentId=$(aws deploy get-deployment-group --application-name calculator_app --deployment-group-name dev_group --query "deploymentGroupInfo.lastAttemptedDeployment.deploymentId" --region ap-southeast-2)
 deploymentGroupId=("${deploymentGroupId: 1:-1}")
 deploymentId=("${deploymentId: 1:-1}")
-cd deployment-root/$deploymentGroupId/$deploymentId/deployment-archive/CalculatorApp
+cd /opt/codedeploy-agent/deployment-root/$deploymentGroupId/$deploymentId/deployment-archive/CalculatorApp
 dotnet publish -o ../build
